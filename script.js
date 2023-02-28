38e74b3954c24872b5a6d010d2f2430127e818b4
AntonioFernandesBR <86790327+AntonioFernandesBR@users.noreply.github.com>
2023-02-28 17:23:45 -0300

    inicio javaScript

---
+++ script.js
@@ -0,0 +1,33 @@
+let fraseQualquer = 'Essa é uma frase qualquer';
+fraseQualquer = 'Outro valor'
+
+const outraFrase = 'Frase'
+
+console.log(fraseQualquer)
+
+
+//Tipos de Dados: STRING
+
+const nossaString = 'Antonio é ';
+const outraString = 'programador';
+
+console.log(nossaString + outraString) //cocatenção
+
+console.log(`Você sabia que o Antonio é ${outraString}`) //Template String coloca o vaor dentro de ${}
+
+//Tipos de Dados: NUMBER
+
+const numero = 3.2; //não usa aspas, só o número
+const numeroA = 2e10; //esse e10 quer dizer que ele tem 10 zeros depois do primeiro número
+
+console.log(typeof numeroA) // esse typeof apresenta no console o tipo de valor>: string, numero, ...
+console.log(numeroA)
+
+//Tipos de Dados: NULL
+
+const valorDaCompra = null;//existe mas é um valor nulo
+
+//Tipos de Dados: BOOLEAN
+
+const boolean = false; //BOOLEAN é ovalor TRUE ou FALSE
+
